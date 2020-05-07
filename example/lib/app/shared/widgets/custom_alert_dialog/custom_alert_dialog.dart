e4670e039a46f74f07228061a58e695e532b7cdf
LucazzP <lucashpolazzo2013@gmail.com>
2020-05-07 12:07:17 -0300

    Added confirm dialog

--- example/lib/app/shared/widgets/custom_alert_dialog/custom_alert_dialog.dart
+++ example/lib/app/shared/widgets/custom_alert_dialog/custom_alert_dialog.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:project_sample_base/app/shared/widgets/custom_alert_dialog/types/device_info_dialog.dart';
+import 'package:project_sample_base/app/shared/widgets/custom_alert_dialog/types/confirm_dialog.dart';
 
 class CustomAlertDialog {
-  static Future<void> deviceInfo(BuildContext context) {
+  static Future<void> confirmDialog(
+    BuildContext context, {
+    String title,
+    Widget content,
+    String option1,
+    String option2,
+    Function onOption1,
+    Function onOption2,
+    TextStyle option1Style,
+    TextStyle option2Style,
+  }) {
     return showDialog(
       context: context,
-      builder: (context) => DeviceInfoDialog(),
+      builder: (context) => ConfirmDialog(
+        title: title,
+        content: content,
+        option1: option1,
+        option2: option2,
+        onOption1: onOption1,
+        onOption2: onOption2,
+        option1Style: option1Style,
+        option2Style: option2Style,
+      ),
     );
   }
 }
